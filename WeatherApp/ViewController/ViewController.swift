eea36c2e8fbc69bec83c0b99042b15a597819264
SungminPark <rnfxl92@naver.com>
2021-02-04 22:05:21 +0900

    load

--- WeatherApp/ViewController/ViewController.swift
+++ WeatherApp/ViewController/ViewController.swift
@@ -23,34 +23,44 @@ class ViewController: UIViewController, ViewModelBindableType {
         viewModel.weatherData
             .drive(listTableView.rx.items(dataSource: viewModel.dataSource))
             .disposed(by: rx.disposeBag)
+        
+        viewModel.weatherData.asObservable()
+            .subscribe(onNext: { [unowned self] _ in
+                self.listTableView.alpha = 1.0
+                self.loader.alpha = 0.0
+            })
+            .dispose()
     }
     
     override func viewDidLoad() {
-       super.viewDidLoad()
-       
-       listTableView.backgroundColor = UIColor.clear
-       listTableView.separatorStyle = .none
-       listTableView.showsVerticalScrollIndicator = false
-       listTableView.allowsSelection = false
+        super.viewDidLoad()
+        
+        loader.alpha = 1.0
+        listTableView.alpha = 0.0
+        
+        listTableView.backgroundColor = UIColor.clear
+        listTableView.separatorStyle = .none
+        listTableView.showsVerticalScrollIndicator = false
+        listTableView.allowsSelection = false
     }
     
     override func viewWillAppear(_ animated: Bool) {
-       super.viewWillAppear(animated)
-       
+        super.viewWillAppear(animated)
+        
     }
-
+    
     var topInset: CGFloat = 0.0
     
     override func viewDidLayoutSubviews() {
-       super.viewDidLayoutSubviews()
-       
-       if topInset == 0.0 {
-          let first = IndexPath(row: 0, section: 0)
-          if let cell = listTableView.cellForRow(at: first) {
-             topInset = listTableView.frame.height - cell.frame.height
-             listTableView.contentInset = UIEdgeInsets(top: topInset, left: 0, bottom: 0, right: 0)
-          }
-       }
+        super.viewDidLayoutSubviews()
+        
+        if topInset == 0.0 {
+            let first = IndexPath(row: 0, section: 0)
+            if let cell = listTableView.cellForRow(at: first) {
+                topInset = listTableView.frame.height - cell.frame.height
+                listTableView.contentInset = UIEdgeInsets(top: topInset, left: 0, bottom: 0, right: 0)
+            }
+        }
     }
-
+    
 }
