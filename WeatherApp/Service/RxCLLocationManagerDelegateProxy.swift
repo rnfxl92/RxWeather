237816743a8659dc3535d764d5ea32ffa57c4cf8
SungminPark <rnfxl92@naver.com>
2021-02-04 17:03:43 +0900

    delegate proxy

---
+++ WeatherApp/Service/RxCLLocationManagerDelegateProxy.swift
@@ -0,0 +1,56 @@
+//
+//  RxCLLocationManagerDelegateProxy.swift
+//  WeatherApp
+//
+//  Created by 박성민 on 2021/02/04.
+//
+
+import UIKit
+import CoreLocation
+import RxSwift
+import RxCocoa
+
+extension CLLocationManager: HasDelegate {
+    public typealias Delegate = CLLocationManagerDelegate
+}
+
+public class RxCLLocationManagerDelegateProxy: DelegateProxy<CLLocationManager, CLLocationManagerDelegate>, DelegateProxyType, CLLocationManagerDelegate {
+    
+    public init(loctionManager: CLLocationManager) {
+        super.init(parentObject: loctionManager, delegateProxy: RxCLLocationManagerDelegateProxy.self)
+    }
+    
+    public static func registerKnownImplementations() {
+        self.register { RxCLLocationManagerDelegateProxy(loctionManager: $0)}
+    }
+}
+
+extension Reactive where Base: CLLocationManager {
+    var delegate: DelegateProxy<CLLocationManager, CLLocationManagerDelegate> {
+        return RxCLLocationManagerDelegateProxy.proxy(for: base)
+    }
+    
+    public var didUpdateLocation: Observable<[CLLocation]> {
+        let sel = #selector(CLLocationManagerDelegate.locationManager(_:didUpdateLocations:))
+        return delegate.methodInvoked(sel)
+            .map { parameters in
+                return parameters[1] as! [CLLocation]
+            }
+    }
+    
+    public var didChangeAuthorizationStatus: Observable<CLAuthorizationStatus> {
+        let sel: Selector
+        
+        if #available(iOS 14.0, *) {
+            sel = #selector(CLLocationManagerDelegate.locationManagerDidChangeAuthorization(_:))
+        } else {
+            sel = #selector(CLLocationManagerDelegate.locationManager(_:didChangeAuthorization:))
+        }
+        
+        return delegate.methodInvoked(sel)
+            .map { parameters in
+                return CLAuthorizationStatus(rawValue: parameters[1] as! Int32) ??
+                    .notDetermined
+            }
+    }
+}
