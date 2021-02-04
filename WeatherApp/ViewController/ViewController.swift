79ad9d932547d9b5a07443dd0def99addc1bdf62
SungminPark <rnfxl92@naver.com>
2021-02-04 16:46:59 +0900

    ViewModel binding

--- WeatherApp/ViewController/ViewController.swift
+++ WeatherApp/ViewController/ViewController.swift
@@ -16,7 +16,13 @@ class ViewController: UIViewController, ViewModelBindableType {
     @IBOutlet weak var listTableView: UITableView!
     
     func bindViewModel() {
-       
+        viewModel.title
+            .bind(to: locationLabel.rx.text)
+            .disposed(by: rx.disposeBag)
+        
+        viewModel.weatherData
+            .drive(listTableView.rx.items(dataSource: viewModel.dataSource))
+            .disposed(by: rx.disposeBag)
     }
     
     override func viewDidLoad() {
